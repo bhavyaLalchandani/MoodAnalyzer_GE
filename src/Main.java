23d075ed13483f1d13eb8a9be42c5f2c4d08d268
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-22 18:02:57 +0530

    UC2 added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         // Test Case 1: Constructor with "I am in Sad Mood"
-        MoodAnalyzer moodAnalyser1 = new MoodAnalyzer("I am in Sad Mood");
+        MoodAnalyzer moodAnalyser1 = new MoodAnalyzer("I am in a Sad Mood");
         String result1 = moodAnalyser1.analyseMood();
         System.out.println("Message: " + moodAnalyser1.getMessage() + " | Mood: " + result1);
 
         // Test Case 1.1: Constructor with "I am in Happy Mood"
-        MoodAnalyzer moodAnalyser2 = new MoodAnalyzer("I am in Happy Mood");
+        MoodAnalyzer moodAnalyser2 = new MoodAnalyzer("I am in a Happy Mood");
         String result2 = moodAnalyser2.analyseMood();
         System.out.println("Message: " + moodAnalyser2.getMessage() + " | Mood: " + result2);
 
